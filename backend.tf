78ec10c63ada00a6ba5955897bd5275e5c96136c
Marcus Martins Leite <mvm.leite@gmail.com>
2025-10-06 00:33:17 -0300

    Deleted file

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-     resource_group_name  = "rg-storage"          # <-- change if you use another RG
-    storage_account_name = "saterraform1754"      # <-- CHANGE to your Storage Account name
-    container_name       = "terraform"
-    key           = minfin-foundation/resource_group.tfstate
-  }
-}
